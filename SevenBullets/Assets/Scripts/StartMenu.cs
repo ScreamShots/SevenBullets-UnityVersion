ca1d205f996eb90620a3b680273e7a2a43d6e1ed
Rémi Sécher <r.secher@rubika-edu.com>
2020-02-03 19:57:19 +0100

    StartScreen

---
+++ SevenBullets/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    [SerializeField] Animator startPanelAnimator;
+    [SerializeField] GameObject startClipingInfo;
+    bool onMenu;
+
+    
+    void Update()
+    {
+        if (Input.anyKeyDown && onMenu == false 
+            && startPanelAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= startPanelAnimator.GetCurrentAnimatorStateInfo(0).length)
+        {
+            StartPanelUp();
+        }
+        else if (Input.GetButtonDown("Cancel") && onMenu == true
+            && startPanelAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= startPanelAnimator.GetCurrentAnimatorStateInfo(0).length)
+        {
+            StartCoroutine(LeaveMenu());
+        }
+    }
+
+    void StartPanelUp()
+    {
+        startClipingInfo.SetActive(false);
+        startPanelAnimator.SetTrigger("anyButtonPressed");
+        onMenu = true;
+    }
+    IEnumerator LeaveMenu()
+    {        
+        startPanelAnimator.SetTrigger("return");
+        yield return new WaitForSeconds(startPanelAnimator.GetCurrentAnimatorStateInfo(0).length + 0.05f);
+        startClipingInfo.SetActive(true);
+        onMenu = false;        
+    }
+
+   
+}
