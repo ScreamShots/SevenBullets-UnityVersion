8ec14dc445aad1517f2ec360d0dab9940c63a64b
Rémi Sécher <r.secher@rubika-edu.com>
2020-02-04 17:40:30 +0100

    StartMenu + Transition + Credits

---
+++ SevenBullets/Assets/Scripts/Credits.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    [SerializeField] Animator enterBlackMelt;
+    [SerializeField] Animator leaveBlackMelt;
+    [SerializeField] GameObject returnButton;
+    bool creditsSet;
+    void Start()
+    {
+        StartCoroutine(ReturnButtonAppear());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Cancel") && creditsSet == true)
+        {
+            StartCoroutine(BackToMenu());
+        }
+    }
+
+    IEnumerator ReturnButtonAppear()
+    {
+        yield return new WaitForSeconds(enterBlackMelt.GetCurrentAnimatorStateInfo(0).length);
+        returnButton.SetActive(true);
+        creditsSet = true;
+    }
+
+    IEnumerator BackToMenu()
+    {
+        leaveBlackMelt.gameObject.SetActive(true);
+        yield return new WaitForSeconds(leaveBlackMelt.GetCurrentAnimatorStateInfo(0).length);
+        SceneManager.LoadScene(0);
+    }
+
+}
--- SevenBullets/Assets/Scripts/StartMenu.cs
+++ SevenBullets/Assets/Scripts/StartMenu.cs
@@ -2,41 +2,100 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
     [SerializeField] Animator startPanelAnimator;
+    [SerializeField] Animator titleAnimator;
+    [SerializeField] Animator allButtonAnimator;
     [SerializeField] GameObject startClipingInfo;
+    [SerializeField] GameObject menuEventSystem;
+    [SerializeField] GameObject blackScreenMelt;
     bool onMenu;
+    bool onLoadAnimation;
 
-    
+    private void Start()
+    {
+        menuEventSystem.SetActive(false);
+        startClipingInfo.SetActive(false);
+        StartCoroutine(EnterStartScreen());
+    }
     void Update()
     {
-        if (Input.anyKeyDown && onMenu == false 
+        if (Input.anyKeyDown && onMenu == false && onLoadAnimation == false
             && startPanelAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= startPanelAnimator.GetCurrentAnimatorStateInfo(0).length)
         {
             StartPanelUp();
         }
-        else if (Input.GetButtonDown("Cancel") && onMenu == true
+        else if (Input.GetButtonDown("Cancel") && onMenu == true && onLoadAnimation == false
             && startPanelAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= startPanelAnimator.GetCurrentAnimatorStateInfo(0).length)
         {
             StartCoroutine(LeaveMenu());
         }
     }
 
+    IEnumerator EnterStartScreen()
+    {        
+        yield return new WaitForSeconds(startPanelAnimator.GetCurrentAnimatorStateInfo(0).length);
+        startClipingInfo.SetActive(true);
+    }
+
     void StartPanelUp()
     {
         startClipingInfo.SetActive(false);
+        menuEventSystem.SetActive(true);
         startPanelAnimator.SetTrigger("anyButtonPressed");
         onMenu = true;
     }
     IEnumerator LeaveMenu()
     {        
         startPanelAnimator.SetTrigger("return");
+        menuEventSystem.SetActive(false);
         yield return new WaitForSeconds(startPanelAnimator.GetCurrentAnimatorStateInfo(0).length + 0.05f);
-        startClipingInfo.SetActive(true);
+        startClipingInfo.SetActive(true);        
         onMenu = false;        
     }
 
+    public void QuitGame()
+    {
+        StartCoroutine(QuitGameTransition());
+    }
    
+
+    IEnumerator QuitGameTransition()
+    {
+        onLoadAnimation = true;
+        Animator blackMeltAnimator = blackScreenMelt.GetComponent<Animator>();
+        blackScreenMelt.SetActive(true);
+        yield return new WaitForSeconds(blackMeltAnimator.GetCurrentAnimatorStateInfo(0).length);
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        StartCoroutine(StartGameTransition());
+    }
+    IEnumerator StartGameTransition()
+    {
+        titleAnimator.SetTrigger("GameLaunched");
+        allButtonAnimator.SetTrigger("GameLaunched");
+        yield return new WaitForSeconds(startPanelAnimator.GetCurrentAnimatorStateInfo(0).length);
+        SceneManager.LoadScene(1);
+    }
+
+    public void Credits()
+    {
+        StartCoroutine(CreditsTransition());
+    }
+
+    IEnumerator CreditsTransition()
+    {
+        onLoadAnimation = true;
+        Animator blackMeltAnimator = blackScreenMelt.GetComponent<Animator>();
+        blackScreenMelt.SetActive(true);
+        yield return new WaitForSeconds(blackMeltAnimator.GetCurrentAnimatorStateInfo(0).length);
+        SceneManager.LoadScene(2);
+    }
+  
 }
