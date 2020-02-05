b2e740220bdd65e13bf6d6fc2a1e7d19d292bdba
Rémi Sécher <r.secher@rubika-edu.com>
2020-02-05 19:48:41 +0100

    starting game

---
+++ SevenBullets/Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    Sprite[] aButton;
+    Sprite[] bButton;
+    Sprite[] xButton;
+    Sprite[] yButton;
+
+    Sprite[][] allButton;
+
+    Sprite basicSprite;
+
+    SpriteRenderer buttonRenderer;
+
+    private void Start()
+    {
+        allButton = new Sprite[4][];
+        allButton[0] = aButton;
+        allButton[1] = bButton;
+        allButton[2] = xButton;
+        allButton[3] = yButton;
+
+        buttonRenderer = GetComponent<SpriteRenderer>();
+    }
+}
